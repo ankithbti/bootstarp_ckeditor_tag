054ac19595a357b6dfe2e21eaa506fd0e9c7cbec
ankithbti <ankithbti007@gmail.com>
2012-11-11 01:04:57 +0530

    Changed model tagging

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :title, :tag_list
+  attr_accessible :content, :title, :tag_list, :tag_ids
   has_many :taggings
 	has_many :tags, through: :taggings
 
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :post_ids
   has_many :taggings
 has_many :posts, through: :taggings
 end
