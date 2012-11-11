6b2c1d4556b49e8396d50fce147ba55e6c95a2e5
ankithbti <ankithbti007@gmail.com>
2012-11-11 15:55:32 +0530

    Updated view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,8 @@ module ApplicationHelper
 	def tag_cloud(tags, classes)
 	  max = tags.sort_by(&:count).last
 	  tags.each do |tag|
-	    index = tag.count / max.count * (classes.size - 1)
+	    index = tag.count.to_f / max.count.to_f * (classes.size - 1)
 	    index = index.round
-	    index = index.to_s
 	    yield(tag, classes[index])
 	  end
 	end
