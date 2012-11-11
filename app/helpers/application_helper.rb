26ee914aa259964c3509c6bd4b9ac5dba7463d29
ankithbti <ankithbti007@gmail.com>
2012-11-11 15:42:54 +0530

    Updated view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
 	  max = tags.sort_by(&:count).last
 	  tags.each do |tag|
 	    index = tag.count.to_f / max.count * (classes.size - 1)
-	    yield(tag, classes[index.round])
+	    yield(tag, classes[index.round.to_s])
 	  end
 	end
 
